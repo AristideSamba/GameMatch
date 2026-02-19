dd4962c95f22d7613aab0636b32611597c7ddf6a
AristideSamba <sambaaristide@gmail.com>
2026-02-19 12:53:55 +0100

    second commit

--- src/gameDetails.jsx
+++ src/gameDetails.jsx
@@ -1,14 +1,18 @@
+import './gamedetails.css';
+
 function GameDetails({ game, onClose }) {
   return (
-    <div style={{ marginTop: "30px", padding: "20px", border: "1px solid #ccc", borderRadius: "8px" }}>
-      <h2>{game.title}</h2>
+    <div className="details">
       <img src={game.thumbnail} alt={game.title} width="300" style={{ marginBottom: "15px" }} />
+      <div className='description'>
+        <h2>{game.title}</h2>
       <p><strong>Description :</strong> {game.short_description || game.description}</p>
       <p>
         <strong>Lien officiel : </strong>
         <a href={game.game_url} target="_blank" rel="noopener noreferrer">Jouer ici</a>
       </p>
       <button onClick={onClose} style={{ marginTop: "10px", padding: "6px 12px" }}>Fermer</button>
+      </div>
     </div>
   );
 }
